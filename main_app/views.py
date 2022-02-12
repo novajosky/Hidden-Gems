c01e7139f4fcf3125ad602bf18ecd1d1e9cfc69c
Michael Novajosky <mikenovajosky@yahoo.com>
2022-02-11 16:24:01 -0800

    create templates

--- main_app/views.py
+++ main_app/views.py
@@ -2,23 +2,15 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
-class Gem:
-    def __init__(self, name, location, description):
-        self.name = name
-        self.location = location
-        self.description = description
-
-gems = {
-    Gem('The Ape Cave', 'Skamania, Washington', 'An awesome cave!'),
-    Gem('Sunnyside Beach', 'Steilacoom, Washington', 'At night, you can see all the stars!')
-}
-
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'base.html')
 
 def about(request):
   return render(request, 'about.html')
 
+def maps(request):
+  return render(request, 'maps.html')
+
 def gems_index(request):
-  return render(request, 'gems/index.html', { 'gems': gems })
+  return render(request, 'gems/index.html')
