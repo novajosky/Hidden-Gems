43c1183985e32e629ab9f152ac14c085499584c7
enkhjino <enkhjin.ochirbal1@gmail.com>
2022-02-14 14:11:17 -0800

    create form

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
   path('maps/', views.maps, name='maps'),
   path('gems/', views.gems_index, name='index'),
   path('accounts/signup/', views.signup, name='signup'),
+  path('gems/<int:gem_id>/', views.gems_detail, name='detail'),
+  path('gems/create/', views.GemCreate.as_view(), name='gems_create'),
+  path('gems/<int:pk>/update/', views.GemUpdate.as_view(), name='gems_update'),
+  path('gems/<int:pk>/delete/', views.GemDelete.as_view(), name='gems_delete'),
 ]
