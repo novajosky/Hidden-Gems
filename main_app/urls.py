4a8c434c1da0b17876f46f36802c9c4b96e24e0b
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-02-16 22:07:22 -0800

    adds fixed slide show

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
   path('maps/', views.maps, name='maps'),
   path('gems/', views.gems_index, name='index'),
   path('accounts/signup/', views.signup, name='signup'),
